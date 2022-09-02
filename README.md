6266c575b2dc946b51d6d3bf4194c6ee42e03392
Anton Tobias Jensen <41019970+Statsministeriet@users.noreply.github.com>
2022-09-02 22:44:59 +0200

    Final testing

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Move from Clodinary to AWS S3? Let's get started 🚀
+# Migrate from Clodinary to AWS S3 🚀
+This script will help you move you Cloudinary files to AWS S3.
 
 ## Getting started
 #### AWS Credentials
 You will need to create an IAM account with S3 Authorization (or full access for the quick and dirty 😉)
 
 #### Cloudinary Credentials
-You can set your Cloudinary credentials in the shell. [See reference here.](https://cloudinary.com/documentation/node_quickstart#set_your_api_environment_variable)
+You can set your Cloudinary credentials in the shell. [See reference](https://cloudinary.com/documentation/node_quickstart#set_your_api_environment_variable)
 
+#### AWS S3 Bucket configurations
+It should be a prerequiste that the bucket policy has ACL Disabled.
+Make sure that Static website hosting is enabled. [Read more](https://docs.aws.amazon.com/AmazonS3/latest/userguide/WebsiteHosting.html)
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Cloudinary = require('cloudinary').v2
+const AWS = require('aws-sdk')
 const util = require('util')
+const axios = require('axios').default;
+const configs = require("./configs.json")
 
-
+const S3Service = new AWS.S3({
+    credentials: {
+        accessKeyId: configs.aws_access_key_id,
+        secretAccessKey: configs.aws_secret_access_key
+    }
+})
 
 const getResource = util.promisify(Cloudinary.api.resources)
 
@@ -20,12 +28,32 @@ const getAllResource = async () => {
             return resources;
         }
     }
+}
 
+const uploadFile = async (resource) => {
+
+    const response = await axios({
+        url: resource.url,
+        method: 'GET',
+        responseType: 'arraybuffer'
+    })
+    const fileName = resource.asset_id +'.'+resource.format
+    const out = await S3Service.upload({
+        ACL: "public-read",
+        Bucket: configs.bucket,
+        Key: fileName,
+        Body: response.data,
+        ContentType: resource.resource_type
+    }).promise()
+    console.log(out)
 }
 
 const main = async () => {
     const resources = await getAllResource()
-    
+    for (let index = 0; index < resources.length; index++) {
+        const element = resources[index];
+        await uploadFile(element) 
+    }
 }
 
 main()
